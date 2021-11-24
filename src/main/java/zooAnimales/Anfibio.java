4db9497b5d1e2fed01f1bf09b519129d529898ec
jero55 <jsalazars@unal.edu.co>
2021-11-23 19:55:20 -0500

    Create Anfibio.java

---
+++ src/main/java/zooAnimales/Anfibio.java
@@ -0,0 +1,55 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+public class Anfibio extends Animal {
+
+	private static ArrayList<Anfibio> listado = new ArrayList<Anfibio>();
+	public static int ranas;
+	public static int salamandras;
+	private String colorPiel;
+	private boolean venenoso;
+	
+	public Anfibio(){listado.add(this);totalAnimales++;}
+	public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
+		super(nombre, edad, habitat, genero);
+		this.colorPiel = colorPiel;
+		this.venenoso = venenoso;
+		listado.add(this);
+		totalAnimales++;
+	}
+	public ArrayList<Anfibio> getListado() {
+		return listado;
+	}
+	public void setListado(ArrayList<Anfibio> listado) {
+		Anfibio.listado = listado;
+	}
+	public String getColorPiel() {
+		return colorPiel;
+	}
+	public void setColorPiel(String colorPiel) {
+		this.colorPiel = colorPiel;
+	}
+	public boolean isVenenoso() {
+		return venenoso;
+	}
+	public void setVenenoso(boolean venenoso) {
+		this.venenoso = venenoso;
+	}
+
+	//
+			
+	public Anfibio crearRana(String nombre,int edad,String genero) {
+		ranas++;	
+		return new Anfibio(nombre,edad,"selva",genero,"rojo",true);
+	}
+
+	public Anfibio crearSalamandra(String nombre,int edad,String genero) {
+		salamandras++;
+		return new Anfibio(nombre,edad,"selva",genero,"negro y amarillo",false);
+	}
+	
+	public static int cantidadAnfibios() {
+		return listado.size();
+	}
+}
